8e7893384f9f20d6da4e12047951488856cf393d
Andrew Hooge <andrewhooge@pushactive.com>
2014-07-23 00:12:17 -0400

    updated specs

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Question, type: :model do
+  let(:question) { subject }
+
+  before { expect(question).not_to be_valid }
+
+  it 'validates the presence of title' do
+    expect(question.errors).to have_key(:title)
+  end
+
+  it 'validates the presence of content' do
+    expect(question.errors).to have_key(:content)
+  end
+end
