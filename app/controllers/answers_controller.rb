b85b30aab5b8ac0264d346026dbc823962bbb8fd
Andrew Hooge <andrewhooge@pushactive.com>
2014-07-23 08:02:54 -0400

    updated controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,24 @@
 class AnswersController < ApplicationController
+  respond_to :html
+
+  def index
+    @answers = Answer.all
+    respond_with (@answers)
+  end
+
+  def new
+    @answer = Answer.new
+    respond_with (@answer)
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+  end
+
+
+  private
+  def answer_params
+    params.require(:answer).permit :question_id, :content
+  end
+
 end
